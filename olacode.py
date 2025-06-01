60b8c4c83c60723396c93a5e81ed0483d3fa2503
olacoding807 <olayemiOnuwaje79@gmail.com>
2025-06-01 09:03:41 -0400

    Add files via upload

---
+++ olacode.py
@@ -0,0 +1,34 @@
+import time
+import random
+
+import difflib
+sentences = [
+    "I love learning Oranges"
+    "I like cows and goats"
+    "I like basketball"
+    "I go to School"
+    "I do coding"
+    "I do not like blueberries"
+    "Python is a type of code",
+]
+
+def Ola():
+    print("Welcome to the typing tutor!/n")
+    sentence = random.choice(sentences)
+    print("Type this sentence:")
+    print(f"/n>>>{sentence}/n")
+    input("Press enter when you are ready to start the typing game")
+    start_time = time.time()
+    typed = input ("|n begin typing:/n>>>")
+    end_time=time.time
+
+    words = len(sentence.split())
+    wpm = round((words / time_taken) * 60)
+    matcher = difflib.SequenceMatcher(None, sentence, typed)
+    accuracy = round(matcher.ratio()*100, 2)
+
+    print("\n--- final Results ---")
+    print("Time taken: {} seconds".format(round(time_taken, 2)))
+    print(f"Your speed: {wpm} WPM")
+    print(f"Accuracy: {accuracy}%")
+Ola()
